07b2e465dc84e6e4136b448e13f47ba4cfab9b73
Asqar <Asqar95>
2023-03-12 16:26:08 +0500

    finished #3

--- cmd/main.go
+++ cmd/main.go
@@ -4,10 +4,15 @@ import (
 	"log"
 	todo "todo-app"
 	"todo-app/pkg/handler"
+	"todo-app/pkg/repository"
+	"todo-app/pkg/service"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+
 	srv := new(todo.Serever)
 	if err := srv.Run("8000", handlers.InnitRouter()); err != nil {
 		log.Fatalf("Error occured while running http server: %s", err.Error())
